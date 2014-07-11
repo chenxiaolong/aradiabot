8e6f89b46580f0154abd401e7aa1f359ee9745b2
Kyle Colantonio <kyle10468@gmail.com>
2014-07-11 18:41:39 -0400

    Updated Main Class
    That one line is apparently a huge difference.

--- src/me/iarekylew00t/ircbot/Main.java
+++ src/me/iarekylew00t/ircbot/Main.java
@@ -14,7 +14,6 @@ import ch.qos.logback.classic.encoder.PatternLayoutEncoder;
 import ch.qos.logback.classic.spi.ILoggingEvent;
 import ch.qos.logback.core.ConsoleAppender;
 import ch.qos.logback.core.FileAppender;
-
 import me.iarekylew00t.utils.FileConfiguration;
 
 /**
