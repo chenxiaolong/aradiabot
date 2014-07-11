09cae29735e5d084b8b65084a3099f0828cd8e42
Kyle Colantonio <kyle10468@gmail.com>
2014-07-11 10:47:49 -0400

    Updated Aradiabot singleton
    Made all methods final.

--- src/me/iarekylew00t/ircbot/Aradiabot.java
+++ src/me/iarekylew00t/ircbot/Aradiabot.java
@@ -26,58 +26,58 @@ public enum Aradiabot {
 	INSTANCE;
 	private static IRCBot BOT;
 	
-	public static final IRCBot getBot() {
-		return BOT;
-	}
-	
-	public static void setBot(IRCBot bot) {
+	public static final void setBot(IRCBot bot) {
 		if (BOT != null) {
-			throw new UnsupportedOperationException("Cannot redefine singleton IRCBot");
+			throw new UnsupportedOperationException("Cannot redefine singleton bot");
 		}
 		BOT = bot;
 		bot.getLogger().info("This bot is running " + getName() + " version " + getVersion() + " (PircBotX API version " + getAPIVersion() + ")");
 	}
+	
+	public static final IRCBot getBot() {
+		return BOT;
+	}
 
-	public static String getName() {
+	public static final String getName() {
 		return BOT.getName();
 	}
-	public static String getNick() {
+	public static final String getNick() {
 		return BOT.getNick();
 	}
 	
-	public static String getVersion() {
+	public static final String getVersion() {
 		return BOT.getVersion();
 	}
 	
-	public static String getAPIVersion() {
+	public static final String getAPIVersion() {
 		return BOT.getAPIVersion();
 	}
 	
-	public static File getPluginDir() {
+	public static final File getPluginDir() {
 		return BOT.getPluginDir();
 	}
 	
-	public static int getBotId() {
+	public static final int getBotId() {
 		return BOT.getBotId();
 	}
 	
-	public static boolean isDebugging() {
+	public static final boolean isDebugging() {
 		return BOT.isDebugging();
 	}
 	
-	public static void setDebug(boolean val) {
+	public static final void setDebug(boolean val) {
 		BOT.setDebug(val);
 	}
 	
-	public static Collection<? extends User> getAllUsers() {
+	public static final Collection<? extends User> getAllUsers() {
 		return BOT.getUserChannelDao().getAllUsers();
 	}
 	
-	public static Collection<? extends User> getUsers(Channel channel) {
+	public static final Collection<? extends User> getUsers(Channel channel) {
 		return BOT.getUserChannelDao().getUsers(channel);
 	}
 	
-	public static User getUser(String user) {
+	public static final User getUser(String user) {
 		for (User u : BOT.getUserChannelDao().getAllUsers()) {
 			if (u.getNick().equals(user)) {
 				return u;
@@ -86,15 +86,15 @@ public enum Aradiabot {
 		return null;
 	}
 
-	public static Collection<? extends Channel> getAllChannels() {
+	public static final Collection<? extends Channel> getAllChannels() {
 		return BOT.getUserChannelDao().getAllChannels();
 	}
 	
-	public static Collection<? extends Channel> getChannels(User user) {
+	public static final Collection<? extends Channel> getChannels(User user) {
 		return BOT.getUserChannelDao().getChannels(user);
 	}
 	
-	public static Channel getChannel(String channel) {
+	public static final Channel getChannel(String channel) {
 		for (Channel c : BOT.getUserChannelDao().getAllChannels()) {
 			if (c.getName().equals(channel)) {
 				return c;
@@ -103,51 +103,51 @@ public enum Aradiabot {
 		return null;
 	}
 	
-	public static String getChannelMode(Channel channel) {
+	public static final String getChannelMode(Channel channel) {
 		return channel.getMode();
 	}
 	
-	public static ImmutableSortedSet<UserLevel> getUserLevels(Channel channel, User user) {
+	public static final ImmutableSortedSet<UserLevel> getUserLevels(Channel channel, User user) {
 		return channel.getUserLevels(user);
 	}
 	
-	public static String getServer() {
+	public static final String getServer() {
 		return BOT.getConfiguration().getServerHostname();
 	}
 	
-	public static int getPort() {
+	public static final int getPort() {
 		return BOT.getConfiguration().getServerPort();
 	}
 	
-	public static List<Command> getCommands() {
+	public static final List<Command> getCommands() {
 		return CommandList.getList();
 	}
 	
-	public static List<IRCPlugin> getPlugins() {
+	public static final List<IRCPlugin> getPlugins() {
 		return PluginList.getList();
 	}
 	
-	public static Logger getLogger() {
+	public static final Logger getLogger() {
 		return BOT.getLogger();
 	}
 	
-	public static void shutdown() {
+	public static final void shutdown() {
 		BOT.shutdown();
 	}
 
-	public static void kick(Channel channel, User user) {
+	public static final void kick(Channel channel, User user) {
 		BOT.kick(channel, user);
 	}
 	
-	public static void kick(Channel channel, User user, String reason) {
+	public static final void kick(Channel channel, User user, String reason) {
 		BOT.kick(channel, user, reason);
 	}
 	
-	public static void kickFromAll(User user) {
+	public static final void kickFromAll(User user) {
 		BOT.kickFromAll(user);
 	}
 	
-	public static void kickFromAll(User user, String reason) {
+	public static final void kickFromAll(User user, String reason) {
 		BOT.kickFromAll(user, reason);
 	}
 	
